a60577f26599f73ed019b2f58274d2ea207fafba
Maxim Samokhvalov <suprocketdev@gmail.com>
2025-03-22 02:29:01 +0300

    fix: show all buttons + js for this func

--- app.js
+++ app.js
@@ -12,3 +12,45 @@ document.querySelector(".overlay").addEventListener("click", function () {
     document.querySelector(".overlay").classList.remove("overlay--active");
     document.querySelector(".aside").classList.remove("aside--active");
 });
+
+const showAllButtons = document.querySelectorAll(".show-all-button");
+
+for(let i = 0; i < showAllButtons.length; i++) {
+    showAllButtons[i].addEventListener("click", function () {
+            const isExpanded = showAllButtons[i].classList.contains("expanded");
+            const dataType = this.getAttribute('data-type');
+            if (dataType === 'show-logos') {
+                if (isExpanded) {
+                    document.querySelectorAll(".slide-logo:nth-of-type(n+7)").forEach(slide => {
+                        slide.setAttribute("style", "display: flex !important;");
+                    });
+                    showAllButtons[i].textContent = "Скрыть";
+                    document.querySelector('.show-all-button.main__content-read-more').classList.add('rotated');
+                    showAllButtons[i].classList.remove("expanded");
+                }else{
+                    document.querySelectorAll(".slide-logo:nth-of-type(n+7)").forEach(slide => {
+                        slide.removeAttribute("style");
+                    });
+                    showAllButtons[i].classList.add("expanded");
+                    showAllButtons[i].classList.remove("rotated");
+                    showAllButtons[i].textContent = "Показать всё";
+                }
+            } else if (dataType === 'show-tech') {
+                if (isExpanded) {
+                    document.querySelectorAll(".slide-tech:nth-of-type(n+4)").forEach(slide => {
+                        slide.setAttribute("style", "display: flex !important;");
+                    });
+                    showAllButtons[i].textContent = "Скрыть";
+                    document.querySelector('#show-tech').classList.add('rotated');
+                    showAllButtons[i].classList.remove("expanded");
+                }else{
+                    document.querySelectorAll(".slide-tech:nth-of-type(n+4)").forEach(slide => {
+                        slide.removeAttribute("style");
+                    });
+                    showAllButtons[i].classList.add("expanded");
+                    showAllButtons[i].classList.remove("rotated");
+                    showAllButtons[i].textContent = "Показать всё";
+                }
+            }
+    })
+}
--- swiper.js
+++ swiper.js
@@ -1,64 +1,68 @@
-let swiperInstance;
+let swiperInstance1, swiperInstance2, swiperInstance3;
 
 function initSwiper() {
-    if (window.innerWidth < 768 && !swiperInstance) {
-        swiperInstance = new Swiper(".swiper", {
-            slidesPerView: 1.3,
-            loop: true,
-            spaceBetween: 16,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: false,
-            },
-        });
-        swiperInstance = new Swiper(".swiper-2", {
-            slidesPerView: 1.3,
-            loop: true,
-            spaceBetween: 16,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: false,
-            },
-        });
-        swiperInstance = new Swiper(".swiper-3", {
-            slidesPerView: 1.4,
-            loop: true,
-            spaceBetween: 20,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: false,
-            },
-        });
+    if (window.innerWidth < 768) {
+        if (!swiperInstance1) {
+            swiperInstance1 = new Swiper(".swiper", {
+                slidesPerView: 1.3,
+                loop: true,
+                spaceBetween: 16,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            });
+        }
 
-    } else if (window.innerWidth > 768 && swiperInstance) {
-        swiperInstance.destroy(true, true);
-        swiperInstance = undefined;
+        if (!swiperInstance2) {
+            swiperInstance2 = new Swiper(".swiper-2", {
+                slidesPerView: 1.5,
+                loop: true,
+                spaceBetween: 30,
+                pagination: {
+                    el: ".swiper-pagination-2",
+                    clickable: true,
+                },
+            });
+        }
+
+        if (!swiperInstance3) {
+            swiperInstance3 = new Swiper(".swiper-3", {
+                slidesPerView: 1.4,
+                loop: true,
+                spaceBetween: 30,
+                pagination: {
+                    el: ".swiper-pagination-3",
+                    clickable: true,
+                },
+            });
+        }
+    } else if (window.innerWidth > 768) {
+        // Уничтожение слайдеров, если ширина экрана больше 768px
+        if (swiperInstance1) {
+            swiperInstance1.destroy(true, true);
+            swiperInstance1 = undefined;
+        }
+        if (swiperInstance2) {
+            swiperInstance2.destroy(true, true);
+            swiperInstance2 = undefined;
+        }
+        if (swiperInstance3) {
+            swiperInstance3.destroy(true, true);
+            swiperInstance3 = undefined;
+        }
     }
 }
 
-// Проверяем при загрузке страницы
+// Проверка при загрузке страницы
 window.addEventListener("load", initSwiper);
 
-// Проверяем при изменении размера экрана
+// Проверка при изменении размера экрана
 window.addEventListener("resize", initSwiper);
 
-const showAllButton = document.querySelector(".show-all-button");
 
-showAllButton.addEventListener("click", () => {
-    const isExpanded = showAllButton.classList.contains("expanded");
-    if (isExpanded) {
-        document.querySelectorAll(".swiper-slide:nth-child(n+7)").forEach(slide => {
-            slide.setAttribute("style", "display: flex !important;");
-        });
-        showAllButton.textContent = "Скрыть";
-        document.querySelector('.show-all-button.main__content-read-more').classList.add('rotated');
-        showAllButton.classList.remove("expanded");
-    }else{
-        document.querySelectorAll(".swiper-slide:nth-child(n+7)").forEach(slide => {
-            slide.removeAttribute("style");
-        });
-        showAllButton.classList.add("expanded");
-        showAllButton.classList.remove("rotated");
-        showAllButton.textContent = "Показать всё";
-    }
-});
+// Проверка при загрузке страницы
+window.addEventListener("load", initSwiper);
+
+// Проверка при изменении размера экрана
+window.addEventListener("resize", initSwiper);
