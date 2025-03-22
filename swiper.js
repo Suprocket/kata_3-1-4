49b4ddf3da53e8a0e9cf922365ee4d617ea5cdc2
Maxim Samokhvalov <suprocketdev@gmail.com>
2025-03-22 17:31:23 +0300

    fix: mobile swiper change slides correctly

--- swiper.js
+++ swiper.js
@@ -2,64 +2,46 @@ let swiperInstance1, swiperInstance2, swiperInstance3;
 
 function initSwiper() {
     if (window.innerWidth < 768) {
-        if (!swiperInstance1) {
-            swiperInstance1 = new Swiper(".swiper", {
-                slidesPerView: 1.3,
-                loop: true,
-                spaceBetween: 16,
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                },
-            });
-        }
-
-        if (!swiperInstance2) {
-            swiperInstance2 = new Swiper(".swiper-2", {
-                slidesPerView: 1.5,
-                loop: true,
-                spaceBetween: 30,
-                pagination: {
-                    el: ".swiper-pagination-2",
-                    clickable: true,
-                },
-            });
-        }
-
-        if (!swiperInstance3) {
-            swiperInstance3 = new Swiper(".swiper-3", {
-                slidesPerView: 1.4,
-                loop: true,
-                spaceBetween: 30,
-                pagination: {
-                    el: ".swiper-pagination-3",
-                    clickable: true,
-                },
-            });
-        }
-    } else if (window.innerWidth > 768) {
-        // Уничтожение слайдеров, если ширина экрана больше 768px
-        if (swiperInstance1) {
-            swiperInstance1.destroy(true, true);
-            swiperInstance1 = undefined;
-        }
-        if (swiperInstance2) {
-            swiperInstance2.destroy(true, true);
-            swiperInstance2 = undefined;
-        }
-        if (swiperInstance3) {
-            swiperInstance3.destroy(true, true);
-            swiperInstance3 = undefined;
-        }
+        swiperInstance1 = new Swiper('.swiper-1', {
+            // Optional parameters
+            slidesPerView: 1.3,
+            slidesPerGroup: 1,
+            loop: true,
+            spaceBetween: 16,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: false,
+            },
+        });
+        swiperInstance2 = new Swiper('.swiper-2', {
+            // Optional parameters
+            slidesPerView: 1.3,
+            spaceBetween: 16,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: false,
+            },
+        });
+        swiperInstance3 = new Swiper('.swiper-3', {
+            // Optional parameters
+            slidesPerView: 1.3,
+            loop: true,
+            spaceBetween: 16,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: false,
+            },
+        });
+    }else{
+        swiperInstance1.destroy(true, true);
+        swiperInstance1 = undefined;
+        swiperInstance2.destroy(true, true);
+        swiperInstance2 = undefined;
+        swiperInstance3.destroy(true, true);
+        swiperInstance3 = undefined;
     }
 }
-
-// Проверка при загрузке страницы
-window.addEventListener("load", initSwiper);
-
-// Проверка при изменении размера экрана
-window.addEventListener("resize", initSwiper);
-
 
 // Проверка при загрузке страницы
 window.addEventListener("load", initSwiper);
