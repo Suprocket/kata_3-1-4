68c7fcb44134071d238c8d6914a3fb0ebeaef4bc
Maxim Samokhvalov <suprocketdev@gmail.com>
2025-03-22 19:28:58 +0300

    fix: swiper.js kiss

--- swiper.js
+++ swiper.js
@@ -1,8 +1,7 @@
 let swiperInstance1, swiperInstance2, swiperInstance3;
 
-function initSwiper() {
-    if (window.innerWidth < 768) {
-        swiperInstance1 = new Swiper('.swiper-1', {
+if (window.innerWidth < 768) {
+    swiperInstance1 = new Swiper('.swiper-1', {
             // Optional parameters
             slidesPerView: 1.3,
             slidesPerGroup: 1,
@@ -33,18 +32,4 @@ function initSwiper() {
                 clickable: false,
             },
         });
-    }else{
-        swiperInstance1.destroy(true, true);
-        swiperInstance1 = undefined;
-        swiperInstance2.destroy(true, true);
-        swiperInstance2 = undefined;
-        swiperInstance3.destroy(true, true);
-        swiperInstance3 = undefined;
-    }
 }
-
-// Проверка при загрузке страницы
-window.addEventListener("load", initSwiper);
-
-// Проверка при изменении размера экрана
-window.addEventListener("resize", initSwiper);
